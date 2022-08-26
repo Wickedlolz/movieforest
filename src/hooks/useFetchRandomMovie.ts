d7989a98fe359d01bc467199456eb13ce90bf1a1
Wickedlolz <iceshotgg.linux@gmail.com>
2022-08-26 10:16:37 +0300

    fix get random movie from global state

--- src/hooks/useFetchRandomMovie.ts
+++ src/hooks/useFetchRandomMovie.ts
@@ -1,11 +1,23 @@
 import { useState, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { moviesState } from '../atoms/moviesAtom';
 import { endpoints, getAllMovies } from '../services/api';
 import { MovieProps } from '../interfaces/movie';
 
 const useFetchRandomMovie = (): MovieProps | null => {
+    const movies = useRecoilValue(moviesState);
     const [movie, setMovie] = useState<MovieProps | null>(null);
 
     useEffect(() => {
+        if (movies) {
+            setMovie(
+                movies.upcoming[
+                    Math.floor(Math.random() * movies.upcoming.length)
+                ]
+            );
+            return;
+        }
+
         getAllMovies(endpoints.UPCOMING)
             .then((result) => {
                 setMovie(
@@ -17,7 +29,7 @@ const useFetchRandomMovie = (): MovieProps | null => {
             .catch((error: any) => {
                 console.log(error.message);
             });
-    }, []);
+    }, [movies]);
 
     return movie;
 };
