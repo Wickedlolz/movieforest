2f2de6a415e96ed6648b4e8e796e38a0748f7232
Wickedlolz <iceshotgg.linux@gmail.com>
2022-09-11 10:43:23 +0300

    finish error boundary

--- src/components/common/ErrorBoundary.tsx
+++ src/components/common/ErrorBoundary.tsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
 interface IState {
     hasError: boolean;
 }
@@ -22,7 +26,36 @@ export default class ErrorBoundary extends Component<any, IState> {
 
     render() {
         if (this.state.hasError) {
-            return <div>ErrorBoundary</div>;
+            return (
+                <Box
+                    sx={{
+                        bgcolor: 'background.paper',
+                        pt: 8,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="red"
+                            gutterBottom
+                            style={{ fontWeight: 700 }}
+                        >
+                            404
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            align="center"
+                            color="text.secondary"
+                            paragraph
+                        >
+                            Something went wrong, try to reaload page.
+                        </Typography>
+                    </Container>
+                </Box>
+            );
         }
 
         return this.props.children;
